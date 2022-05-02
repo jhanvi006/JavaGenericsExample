3e3c2772ef09c4e335e0d5cda1dbb80978abb257
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-02 16:02:54 -0400

    Added: find max value of 3 STrings added

--- src/com/bridgelabz/FindMaxValueUsingGenerics.java
+++ src/com/bridgelabz/FindMaxValueUsingGenerics.java
@@ -22,8 +22,17 @@ public class FindMaxValueUsingGenerics {
             max = num3;
         System.out.println("Maximum of "+num1+", "+num2+", "+num3+" is "+max);
     }
+    public static void findMaxValue(String str1, String str2, String str3){
+        String max = str1;
+        if (str2.compareTo(max)>0)
+            max = str2;
+        if (str3.compareTo(max)>0)
+            max = str3;
+        System.out.println("Maximum of "+str1+", "+str2+", "+str3+" is "+max);
+    }
     public static void main(String[] args) {
         FindMaxValueUsingGenerics.findMaxValue(20, 12, 15);
         FindMaxValueUsingGenerics.findMaxValue(20.3f, 22.1f, 15.9f);
+        FindMaxValueUsingGenerics.findMaxValue("Apple", "Peach", "Banana");
     }
 }

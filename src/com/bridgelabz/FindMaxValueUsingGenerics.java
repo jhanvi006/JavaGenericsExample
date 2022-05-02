ceeab30ae617a182a55dbe01a7c8587d66b90af6
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-02 16:15:00 -0400

    Modified: methods with different wrapper class are replaced with generic method

--- src/com/bridgelabz/FindMaxValueUsingGenerics.java
+++ src/com/bridgelabz/FindMaxValueUsingGenerics.java
@@ -6,33 +6,25 @@ package com.bridgelabz;
  * @since - 2022-05-02
  */
 public class FindMaxValueUsingGenerics {
-    public static void findMaxValue(Integer num1, Integer num2, Integer num3){
-        Integer max = num1;
-        if (num2.compareTo(max)>0)
-            max = num2;
-        if (num3.compareTo(max)>0)
-            max = num3;
-        System.out.println("Maximum of "+num1+", "+num2+", "+num3+" is "+max);
-    }
-    public static void findMaxValue(Float num1, Float num2, Float num3){
-        Float max = num1;
-        if (num2.compareTo(max)>0)
-            max = num2;
-        if (num3.compareTo(max)>0)
-            max = num3;
-        System.out.println("Maximum of "+num1+", "+num2+", "+num3+" is "+max);
-    }
-    public static void findMaxValue(String str1, String str2, String str3){
-        String max = str1;
-        if (str2.compareTo(max)>0)
-            max = str2;
-        if (str3.compareTo(max)>0)
-            max = str3;
-        System.out.println("Maximum of "+str1+", "+str2+", "+str3+" is "+max);
+    /**
+     * Generic method to compare any type of data and find maximum
+     * @param a1 : First value to compare
+     * @param a2 : Second value to compare
+     * @param a3 : Third value to compare
+     * @return max : Maximum of three values
+     */
+    public static <T extends Comparable<T>> T findMaxValue(T a1, T a2, T a3){
+        T max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
     }
     public static void main(String[] args) {
-        FindMaxValueUsingGenerics.findMaxValue(20, 12, 15);
-        FindMaxValueUsingGenerics.findMaxValue(20.3f, 22.1f, 15.9f);
-        FindMaxValueUsingGenerics.findMaxValue("Apple", "Peach", "Banana");
+        System.out.println("Welcome to Find Maximum Problem Using Generics.");
+        System.out.println("The maximum value between the three Integers is : "+findMaxValue(20, 12, 15));
+        System.out.println("The maximum value between the three Floats is : "+findMaxValue(20.3f, 22.1f, 15.9f));
+        System.out.println("The maximum value between the three Strings is : "+findMaxValue("Apple", "Peach", "Banana"));
     }
 }
